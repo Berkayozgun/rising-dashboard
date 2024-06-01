f244c4f696c0ab447565fc32c058a2be5df5f92b
Berkay Özgün <berkayozgun001@gmail.com>
2024-06-01 20:20:53 +0300

    fix : dropdown onchange number of ip value on browser console

--- src/app/components/TransactionRow.tsx
+++ src/app/components/TransactionRow.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
 function TransactionRow({ item }) {
+  const handleActionClick = (item) => (e) => {
+    console.log(`Number of IP: ${item.ipcount}`)
+  }
+
   return (
     <tr className='bg-white border-b'>
     <td className=''>{item.type}</td>
@@ -10,7 +14,8 @@ function TransactionRow({ item }) {
     <td className=''>{item.purpose}</td>
     <td className=''>{item.date}</td>
     <td>
-      <select className='w-28 h-12 rounded-xl m-2 border-none drop-shadow-xl text-sm'>
+      <select className='w-28 h-12 rounded-xl m-2 border-none drop-shadow-xl text-sm'
+      onChange={handleActionClick(item)}>
         <option value='1'>Actions</option>
         <option value='2'>Processing</option>
         <option value='3'>In Progress</option>
